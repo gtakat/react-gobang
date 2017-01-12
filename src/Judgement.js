07e8a04d7884fd7d08431f3d4e995530e0bc2121
gtakat <gtakat@gmail.com>
2017-01-12 11:00:14 +0900

    add slanting check 1

--- src/Judgement.js
+++ src/Judgement.js
@@ -47,12 +47,12 @@ class Judgement {
     // horizontal and vertical line check
     let horizontalCount = 0;
     let verticalCount = 0;
-    
-    for (let i=0; i<=17; i++) {
+
+    for (let i=0; i<=18; i++) {
       // horizontal line check
       if (squares[row][i] === color) {
         horizontalCount++;
-        if ((horizontalCount === 5) && (squares[row][i+1] !== color)) {
+        if ((horizontalCount === 5) && (i === 18 || (squares[row][i+1] !== color))) {
           return true;
         }
       } else {
@@ -62,7 +62,7 @@ class Judgement {
       // vertical line check
       if (squares[i][col] === color) {
         verticalCount++
-        if ((verticalCount === 5) && (squares[i+1][col] !== color)) {
+        if ((verticalCount === 5) && (i === 18 || (squares[i+1][col] !== color))) {
           return true;
         }
       } else {
@@ -75,8 +75,32 @@ class Judgement {
     // slanting line check
     let slantingCount = 0;
 
-    // slanting line check 1
-    // slanting line check 2
+    // slanting line check 1 (left up to right down direction)
+    let horizontalOffset = row > col ? 0 : col - row;
+    let verticalOffset = row > col ? row - col : 0;
+
+    for (let i=0; i<=18; i++) {
+      let squareEnd = false;
+      if (((i + horizontalOffset) === 18) || ((i + verticalOffset) === 18)) {
+        squareEnd = true;
+      }
+
+      if (squares[i+horizontalOffset][i+verticalOffset] === color) {
+        slantingCount++;
+        if ((slantingCount === 5) && (squareEnd || (squares[i+horizontalOffset+1][i+verticalOffset+1] !== color))) {
+          return true;
+        }
+
+      } else {
+        slantingCount = 0;
+      }
+
+      if (squareEnd) {
+        break;
+      }
+    }
+
+    // slanting line check 2 (left down to right up direction)
 
     return false;
   }
