5cd525e92dec4e225f86e9ae1a9a3fb55d1c1a22
gtakat <gtakat@gmail.com>
2017-02-06 20:54:44 +0900

    add click test

--- src/App.test.js
+++ src/App.test.js
@@ -50,14 +50,18 @@ describe('<Game>', () => {
     ])).to.equal(true);
   });
 
-  describe('click masu', () => {
-    it('click masu for win', () => {
-      let squares = Array(19);
+  describe('hit goishi', () => {
+
+    let squares;
+    let wrapper;
+
+    beforeAll(() => {
+      squares = new Array(19);
       for (let i=0; i<squares.length; i++) {
-        squares[i] = Array(19).fill(null);
+        squares[i] = (new Array(19)).fill(null);
       }
 
-      const wrapper = mount(<Game />);
+      wrapper = mount(<Game />);
 
       wrapper.find('#App-masu-9-9').simulate('click');  // black
       wrapper.find('#App-masu-9-10').simulate('click'); // white
@@ -67,10 +71,23 @@ describe('<Game>', () => {
       wrapper.find('#App-masu-11-2').simulate('click'); // white
       wrapper.find('#App-masu-6-9').simulate('click');  // black
       wrapper.find('#App-masu-13-2').simulate('click'); // white
-      wrapper.find('#App-masu-5-9').simulate('click');  // black (win)
+    });
 
+    it("click masu already exist (don't click)", () => {
+      wrapper.find('#App-masu-6-9').simulate('click');  // black (don't click)
+      expect(wrapper.find(Goban).find('.App-goishi-black')).to.have.length(4);
+    });
+
+    it('click masu for win', () => {
+      wrapper.find('#App-masu-5-9').simulate('click');  // black (win)
       expect(wrapper.find('#App-game-info-left').text()).to.equal('Winner! :');
-    })
+    });
+
+    it("cilck masu after win (don't click)", () => {
+      wrapper.find('#App-masu-5-9').simulate('click');  // black (win)
+      wrapper.find('#App-masu-5-10').simulate('click'); // white (don't click)
+      expect(wrapper.find(Goban).find('.App-goishi-white')).to.have.length(4);
+    });
   });
 });
 
